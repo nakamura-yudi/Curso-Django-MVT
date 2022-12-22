23dedb528d82787ccbc260a7cf7650b3c0fe6aea
Eduardo Yudi Nakamura <64866477+nakamura-yudi@users.noreply.github.com>
2022-12-22 13:56:53 -0300

    Update settings.py

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receitas',
+        'USER':'postgres',
+        'PASSWORD':'1pw1s3',
+        'HOST':'localhost',
     }
 }
 
