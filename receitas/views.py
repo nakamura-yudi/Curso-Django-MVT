872dc07150508615fdd4b65a07fb1606a3d8d18d
Eduardo Yudi Nakamura <64866477+nakamura-yudi@users.noreply.github.com>
2022-12-23 08:28:18 -0300

    conclusão

--- receitas/views.py
+++ receitas/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Receita
 
 def index(request):
+    receitas = Receita.objects.all() 
+    return render(request, 'index.html', {'receitas':receitas})
 
-    receitas = {
-        1:'Lasanha',
-        2:'Sopa de legumes',
-        3:'Sorvete',
-        4:'bolo de chocolate'
-    }
-
-    dados = {
-        'nome_das_receitas' : receitas
-    }
-    return render(request, 'index.html',dados)
-
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request,receita_id):
+    receita = Receita.objects.get(pk=receita_id)
+    return render(request, 'receita.html', {'receita':receita})
