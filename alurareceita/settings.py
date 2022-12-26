0d41dab7eab79cee6a25bf2c390ffe3dfe238085
Eduardo Yudi Nakamura <64866477+nakamura-yudi@users.noreply.github.com>
2022-12-26 16:34:20 -0300

    a

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'receitas',
+    'pessoas',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -121,10 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'alurareceita/static')
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
